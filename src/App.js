1af9ab1f8d19346720639c0c95e9c8cbabbcfb10
balajin96 <balajinatarajan18011996@gmail.com>
2022-06-21 08:52:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import Cart from './containers/Cart';
-import ProductList from './containers/ProductList';
-
+import React from "react";
+import Cart from "./containers/Cart";
+import ProductList from "./containers/ProductList";
 const App = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>Simple E-Commerce Shopping cart</h1>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-8">
-                    <ProductList />
-                </div>
-                <div className="col-md-4">
-                    <Cart />
-                </div>
-            </div>
+  return (
+    <div className="container" >
+      <div className="row" >
+        <div className="col-md-12">
+          <h1>Simple ToyShop E-cart</h1>
         </div>
-    );
-}
+      </div>
+      <div className="row">
+        <div className="col-md-8">
+          <ProductList />
+        </div>
+        <div className="col-md-4">
+          <Cart />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
